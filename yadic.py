e84406e5e3788f2fba3dc40851c7712eda28f5ed
gguman <gguman@rambler.ru>
2019-01-18 22:58:55 +0300

    rewrite to re module

--- yadic.py
+++ yadic.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import requests, json, platform, os
+import requests, json, platform, os, re
 
 # Получить токен можно по этой ссылке: https://translate.yandex.ru/developers/keys
 # Проверить количество переведенных символов в Статистике: https://translate.yandex.ru/developers/stat
@@ -19,20 +19,29 @@ def translate(eng_text):
     else:
         print(f"\nError. Find the reference for code { _result.get('code')} here:\nhttps://tech.yandex.ru/translate/doc/dg/reference/translate-docpage/") 
 
-if __name__ == '__main__':
+def grab_text():
+    '''
+    documentation must be 
+        here in the future...
+    '''
+
     print('\nWellcome to our online dictionary!\nref: input (t)ext. catch (r)esult. eeasy')
     _cl = 'clear' if platform.system() == 'Linux' else 'cls'
     _cnt = 1
     while True:
         _text = input(f'\n[{_cnt}] t: ' )
         if len(_text) > 0:
-            if _text == 'exit()':
-                exit()
-            elif _text == 'clear()':
-                _clear = lambda: os.system(_cl)
-                _clear()
-                _cnt -= 1
+            if re.search('^!', _text):
+                if re.search('exit', _text): exit()
+                if re.search('clear', _text): _clear = lambda: os.system(_cl); _clear(); _cnt -= 1
+                if re.search('help', _text): print(grab_text.__doc__); _cnt -= 1
             else:
                 _result = translate(_text)
                 print(f"[{_cnt}] r: {_result}")
+                
             _cnt += 1
+
+
+if __name__ == '__main__':
+    #print(grab_text.__doc__)
+    grab_text()
